f6c72ec34d8814c438659a05a5f0e765a0ddef04
sai-n20 <sai2010nadkarni@gmail.com>
2020-11-22 01:49:13 +0530

    initialize priority queue

--- cpath.cpp
+++ cpath.cpp
@@ -6,19 +6,34 @@ int main(int argc, char *argv[]){
   graph g;
   std::vector<graph::vertex_label> ccsp_rpt;
   std::vector<graph::vertex_label> ctsp_rpt;
+  unsigned int source;
+  unsigned int dest;
+  unsigned int budget;
 
-  if(argc != 2) 
-    std::cout << "Usage: cpath <file> <s> <d> <budget>\n";
-  else {
-    if(!g.read_file(argv[1]))
-      std::cout << "Could not open file '" << argv[1] << "'\n";
+  if(argc < 5) {
+    	std::cout << "Usage: cpath <file> <s> <d> <budget>\n";
+    	return 0;
   }
+  else {
+    if(!g.read_file(argv[1])) {
+    	std::cout << "Could not open file '" << argv[1] << "'\n";
+		return 0;
+	}
+  }
+
+  source = atoi(argv[2]);
+  dest = atoi(argv[3]);
+  budget = atoi(argv[4]);
 
   std::cout << "\nADJACENCY-LIST REPRESENTATION:\n\n";
   g.display();
+  g.createPQ();
 
-  int costList[] = {10, 14, 15, 18, 26};
-  int timeList[] = {34, 15, 26, 19, 22};
+  // inputGraph = g.returnGraph();
+  // std::cout<< inputGraph[0].outgoing;
+  
+  // int costList[] = {10, 14, 15, 18, 26};
+  // int timeList[] = {34, 15, 26, 19, 22};
 
 //   if(g.topo_sort(id_order)) 
 //     std::cout << "topo sort succeeded!\n";
