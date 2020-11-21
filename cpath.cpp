b9312b6a22184c640e45b7562fd33607a7b1c40d
sai-n20 <sai2010nadkarni@gmail.com>
2020-11-21 23:25:45 +0530

    initial file

---
+++ cpath.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Graph.h"
+
+
+int main(int argc, char *argv[]){
+  graph g;
+  std::vector<graph::vertex_label> ccsp_rpt;
+  std::vector<graph::vertex_label> ctsp_rpt;
+
+  if(argc != 2) 
+    std::cout << "Usage: cpath <file> <s> <d> <budget>\n";
+  else {
+    if(!g.read_file(argv[1]))
+      std::cout << "Could not open file '" << argv[1] << "'\n";
+  }
+
+  std::cout << "\nADJACENCY-LIST REPRESENTATION:\n\n";
+  g.display();
+
+  int costList[] = {10, 14, 15, 18, 26};
+  int timeList[] = {34, 15, 26, 19, 22};
+
+//   if(g.topo_sort(id_order)) 
+//     std::cout << "topo sort succeeded!\n";
+//   else 
+//     std::cout << "topo sort failed (cycle?)!\n";
+
+//   g.ids2names(id_order, name_order);
+
+//   std::cout << "here is the topological order produced:\n\n";
+
+//   pvec(name_order);
+
+
+  return 0;
+}
+
