a72f2986242fc6b5f20b59f6222cd06053622a4a
sai-n20 <sai2010nadkarni@gmail.com>
2020-11-22 23:46:32 +0530

    formatting

--- cpath.cpp
+++ cpath.cpp
@@ -2,7 +2,7 @@
 #include "Graph.h"
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char* argv[]) {
   graph g;
   std::vector<graph::vertex_label> ccsp_rpt;
   std::vector<graph::vertex_label> ctsp_rpt;
@@ -10,23 +10,23 @@ int main(int argc, char *argv[]){
   unsigned int dest;
   unsigned int budget;
 
-  if(argc < 5) {
-    	std::cout << "Usage: cpath <file> <s> <d> <budget>\n";
-    	return 0;
+  if (argc < 5) {
+    std::cout << "Usage: cpath <file> <s> <d> <budget>\n";
+    return 0;
   }
   else {
-    if(!g.read_file(argv[1])) {
-    	std::cout << "Could not open file '" << argv[1] << "'\n";
-		return 0;
-	}
+    if (!g.read_file(argv[1])) {
+      std::cout << "Could not open file '" << argv[1] << "'\n";
+      return 0;
+    }
   }
 
   source = atoi(argv[2]);
   dest = atoi(argv[3]);
   budget = atoi(argv[4]);
 
-  std::cout << "\nADJACENCY-LIST REPRESENTATION:\n\n";
-  g.display();
+  // std::cout << "\nADJACENCY-LIST REPRESENTATION:\n\n";
+  // g.display();
   g.createPQ();
 
   return 0;
